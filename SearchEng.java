d990fc5f65514a807b521ad3c0807ac12dbb026b
Mashael232 <w6123w@gmail.com>
2024-11-23 19:27:11 +0300

    Update SearchEng.java

--- SearchEng.java
+++ SearchEng.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.ds;
 
-/**
- *
- * @author jojo0
- */
+
 import java.io.File;
 import java.util.Scanner;
 public class SearchEng {
